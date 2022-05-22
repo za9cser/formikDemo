816cdd27f2d8bdecff54e9525543ee0440979335
Sergei Zaitsev <sz@versta24.ru>
2022-05-22 18:08:29 +0300

    formfeedback

--- fomikdemo/src/modules/order/index.js
+++ fomikdemo/src/modules/order/index.js
@@ -1,7 +1,7 @@
 import { TextField } from "@mui/material";
 import { useFormik } from "formik";
 import React, { useState } from "react";
-import { Alert, Button, Col, Form, Row } from "reactstrap";
+import { Alert, Button, Col, Form, FormFeedback, FormGroup, FormText, Row } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
@@ -19,9 +19,9 @@ const Order = () => {
         validate: (values) => {
             const errors = {};
             if (values.cityFrom === "") errors.cityFrom = "Поле 'Откуда' не заполненно";
+            else if (values.cityFrom.length < 3) errors.cityFrom = "В поле 'Откуда' должно быть минимум 3 символа";
             if (values.cityTo === "") errors.cityTo = "Поле 'Куда' не заполненно";
-            if (values.cityFrom.length < 3) errors.cityFrom = "В поле 'Откуда' должно быть минимум 3 символа";
-            if (values.cityTo.length < 3) errors.cityTo = "В поле 'Куда' должно быть минимум 3 символа";
+            else if (values.cityTo.length < 3) errors.cityTo = "В поле 'Куда' должно быть минимум 3 символа";
             return errors;
         },
         onSubmit: (values, { setSubmitting }) => {
@@ -42,17 +42,21 @@ const Order = () => {
                 <h5>Новый заказ</h5>
             </Row>
             <Row className="my-3">
-                <TextField
-                    name="cityFrom"
-                    label="Откуда"
-                    placeholder="Откуда"
-                    size="small"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.cityFrom}
-                    disabled={formik.isSubmitting}
-                    autoComplete="off"
-                />
+                <FormGroup>
+                    <TextField
+                        name="cityFrom"
+                        label="Откуда"
+                        placeholder="Откуда"
+                        size="small"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.cityFrom}
+                        disabled={formik.isSubmitting}
+                        error={formik.errors.cityFrom}                        
+                        autoComplete="off"
+                    />
+                    {formik.errors.cityFrom && <Form valid={false}>{formik.errors.cityFrom}</FormFeedback>}
+                </FormGroup>
             </Row>
             <Row className="my-3">
                 <TextField
@@ -66,6 +70,7 @@ const Order = () => {
                     disabled={formik.isSubmitting}
                     autoComplete="off"
                 />
+                {formik.errors.cityTo && <FormText className="text-danger">{formik.errors.cityTo}</FormText>}
             </Row>
             <Row className="my-3 justify-content-around">
                 <Col className={isSubmitted ? "text-end" : "text-center"}>
