e2f471e06c084770efa0393d5462df8acc0e0f43
Sergei Zaitsev <sz@versta24.ru>
2022-05-22 15:58:27 +0300

    form reset

--- fomikdemo/src/modules/order/index.js
+++ fomikdemo/src/modules/order/index.js
@@ -1,7 +1,9 @@
 import { TextField } from "@mui/material";
 import { useFormik } from "formik";
 import React, { useState } from "react";
-import { Alert, Button, Form, FormGroup, Input, Label, Row } from "reactstrap";
+import { Alert, Button, Col, Form, FormGroup, Input, Label, Row } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 const initialValues = {
     cityFrom: "",
@@ -9,18 +11,20 @@ const initialValues = {
 };
 
 const Order = () => {
+    const [isSubmitted, setIsSubmitted] = useState(false);
     const [showAlert, setShowAlert] = useState(false);
     const [message, setMessage] = useState("");
     const formik = useFormik({
         initialValues: initialValues,
         onSubmit: (values, { setSubmitting }) => {
             setShowAlert(false);
-
+            setIsSubmitted(false);
             setTimeout(() => {
                 setMessage(`Создано: ${values.cityFrom} -> ${values.cityTo}`);
                 setSubmitting(false);
                 setShowAlert(true);
-            }, 2000);
+                setIsSubmitted(true);
+            }, 4000);
         },
     });
 
@@ -38,6 +42,7 @@ const Order = () => {
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
                     value={formik.values.cityFrom}
+                    disabled={formik.isSubmitting}
                     autoComplete="off"
                 />
             </Row>
@@ -50,11 +55,33 @@ const Order = () => {
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
                     value={formik.values.cityTo}
+                    disabled={formik.isSubmitting}
                     autoComplete="off"
                 />
             </Row>
-            <Row className="my-3">
-                <Button type="submit">Создать</Button>
+            <Row className="my-3 justify-content-around">
+                <Col className={isSubmitted ? "text-end" : "text-center"}>
+                    <Button type="submit" color="primary" disabled={formik.isSubmitting}>
+                        {formik.isSubmitting && <FontAwesomeIcon icon={faSpinner} spin className="me-1" />}
+                        Создать
+                    </Button>
+                </Col>
+
+                {isSubmitted && (
+                    <Col className="text-start">
+                        <Button
+                            type="button"
+                            disabled={formik.isSubmitting}
+                            onClick={() => {
+                                setShowAlert(false);
+                                setIsSubmitted(false);
+                                formik.resetForm();
+                            }}
+                        >
+                            Очистить
+                        </Button>
+                    </Col>
+                )}
             </Row>
             <Row>
                 <Alert isOpen={showAlert} color="success">
