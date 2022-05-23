4dffb0c28347a33bc69094b0442a12a4a146a037
Sergei Zaitsev <sz@versta24.ru>
2022-05-23 23:11:00 +0300

    theme

--- fomikdemo/src/Layout.js
+++ fomikdemo/src/Layout.js
@@ -1,6 +1,21 @@
 import React from "react";
 import { Container } from "reactstrap";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+    components: {
+        MuiFormLabel: {
+            styleOverrides: {
+                asterisk: { color: "red" },
+            },
+        },
+    },
+});
 
 export default function Layout({ children }) {
-  return <Container className="py-5">{children}</Container>;
+    return (
+        <Container className="py-5">
+            <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </Container>
+    );
 }
